b63cf31443070b695b8174f454197500ec652712
mejdoubi meriem <mejdoubi.meriem@gmail.Com>
2022-01-19 20:35:44 +0100

    Create PremierBlock.php

---
+++ src/Plugin/Block/PremierBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\mm_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a block with a simple text.
+ *
+ * @Block(
+ *   id = "mm_blocks_premier_block",
+ *   admin_label = @Translation("My first block to display date"),
+ * )
+ */
+class PremierBlock extends BlockBase
+{
+    
+    public function build(): array 
+    {
+        return ['#markup' =>  strftime('%Y-%m-%d %H:%M'),];
+    }
+    
+    protected function blockAccess(AccountInterface $account)
+    {
+        return AccessResult::allowedIfHasPermission($account, 'access content');
+    }
+  
+}
